c4b1b1f298ad48401fee1ebc8147f4046ac66ce4
JG <JG@DESKTOP-UA3G2K4>
2019-08-15 19:56:58 -0400

    Migracion sql

---
+++ Api_p2/Persistence/Migrations/20190815235016_Initialize.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    personasid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.personasid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
---
+++ Api_p2/Persistence/PersonasDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class PersonasDbcontext : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        public PersonasDbcontext(DbContextOptions<PersonasDbcontext> options)
+                : base(options)
+        {
+
+        }
+    }
+}
